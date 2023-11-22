12cb4273665bb3baafa166d1d14a6eb94f4253cc
jojunhan <151700463+jojunhan@users.noreply.github.com>
2023-11-22 11:40:28 +0900

    Add files via upload

---
+++ streamlit_chatbot.py
@@ -0,0 +1,43 @@
+import streamlit as st
+import openai
+
+st.title("Chatbot with OpenAI GPT")
+st.caption("Made by wisdom")
+
+with st.sidebar:
+    st.markdown("[Get an Open AI key](https://platform.openai.com/account/api-keys)")
+
+#client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
+client = openai(api_key=st.secrets["OPENAI_API_KEY"])
+st.write(st.secrets["nonexistent_key"])
+
+if "openai_model" not in st.session_state:
+    st.session_state["openai_model"] = "gpt-3.5-turbo"
+
+if "messages" not in st.session_state:
+    st.session_state.mesages = []
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+if prompt := st.chat_input("What is up?"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    with st.chat_message("user"):
+        st.markdown(prompt)
+
+    with st.chat_message("assistant"):
+        message_placeholder = st.empty()
+        full_response = ""
+        for response in client.chat.completions.create(
+            model=st.session_state["openai_model"],
+            mesages=[
+                {"role": m["role"], "content" : m["content"]}
+                for m in st.session_state.messages
+            ],
+            stream=True,
+        ):
+            full_response += (response.choices[0].delta.content or "")
+            message_placeholder.markdown(full_response + "▌")
+        message_placeholder.markdown(full_response)
+    st.session_state.messages.append({"role": "assistant", "content": full_response})
